1a46728d05862aa85b0f837ef036e700484238b0
Nicolás Marulanda P <nicomaru@outlook.com>
2018-07-26 23:07:13 +0200

    Script para agregar nuevos módulos - Correcciones.

--- views/js/properties.js
+++ views/js/properties.js
@@ -1,62 +1,12 @@
+var inputProjectModules;
+
 (function () {
+    inputProjectModules = $(document).find('#input-project-modules');
+    $(document).on('click', '#modal-btn-add', function () {
+        btnAddFormAddModule();
+    });
     $(document).on('click', '#modal-btn-edit', function () {
-        var formEditProperties = $(document).find('#form-edit-properties');
-        var inputProjectModules = $(document).find('#input-project-modules');
-        var modalFormAddModule = $(document).find('#modal-form-add-module');
-        var inputTemplateReplace = modalFormAddModule.find('#input-project-classes-template-replace');
-        var inputProjectModuleId = $(document).find('#input-project-module-id');
-        var replaceObj = {};
-        var moduleObjects = [];
-        var moduleObj = {};
-
-        if (inputProjectModules.val().length > 0) {
-            moduleObjects = JSON.parse(inputProjectModules.val());
-        }
-
-
-        modalFormAddModule.find('#nav-add-module-data > .form-group').each(function () {
-            var inputHidden = $(this).find('input[type=hidden]');
-            var inputText = $(this).find('input[type=text]');
-
-            if (inputHidden.length > 0 && inputText.length > 0) {
-                replaceObj[inputHidden.val()] = inputText.val()
-            }
-        });
-        inputTemplateReplace.val(JSON.stringify(replaceObj));
-
-        var modalFormSerialize = $(document).find('#modal-form-add-module').serializeArray();
-
-        for (var i = 0, len = modalFormSerialize.length; i < len; i++) {
-            var name = modalFormSerialize[i]["name"];
-            var value = modalFormSerialize[i]["value"];
-
-            moduleObj[name] = value;
-        }
-
-        //Si no tiene ID se le establece uno y se agrega a la lista de módulos
-        if (inputProjectModuleId.val().length === 0) {
-            var id = 1;
-
-            if (moduleObjects.length > 0) {
-                var lastModule = moduleObjects[moduleObjects.length - 1];
-                id = parseInt(lastModule["projectModuleId"]) + 1;
-            }
-
-            moduleObj["projectModuleId"] = id;
-
-            moduleObjects = moduleObjects.concat(moduleObj);
-        } else {
-            for (var i = 0, len = moduleObjects.length; i < len; i++) {
-                if (moduleObjects[i]["projectModuleId"] == inputProjectModuleId.val()) {
-                    moduleObjects[i] = moduleObj;
-                    break;
-                }
-            }
-        }
-
-        inputProjectModules.val(JSON.stringify(moduleObjects));
-
-        console.log(formEditProperties.serializeArray());
+        btnEditFormAddModule($(document).find('#input-project-module-id'));
     });
     $(document).on('click', '.modal-btn-replace-delete', function () {
         $(this).closest('.form-group').remove();
@@ -70,20 +20,101 @@
             pos = parseInt(lastInput.val()) + 1;
         }
 
-        var html = '<div class="form-group">\n' +
-            '<input type="hidden" value="' + pos + '"/>\n' +
-            '<div class="input-group mb-3">\n' +
-            '<div class="input-group-prepend">\n' +
-            '<span class="input-group-text">%' + pos + '$s</span>\n' +
-            '</div>\n' +
-            '<input class="form-control" type="text" placeholder="Test001">\n' +
-            '<div class="input-group-append">\n' +
-            '<button class="btn btn-danger modal-btn-replace-delete" type="button">\n' +
-            'Eliminar\n' +
-            '</button>\n' +
-            '</div>\n' +
-            '</div>\n' +
-            '</div>';
-        elementNav.append(html);
+        elementNav.append(getTemplateReplaceHtml(pos));
     });
 })();
+
+function getTemplateReplaceHtml(pos) {
+    //TODO: mover a una plantilla.
+    return '<div class="form-group">\n' +
+        '<input type="hidden" value="' + pos + '"/>\n' +
+        '<div class="input-group mb-3">\n' +
+        '<div class="input-group-prepend">\n' +
+        '<span class="input-group-text">%' + pos + '$s</span>\n' +
+        '</div>\n' +
+        '<input class="form-control" type="text" placeholder="Test001">\n' +
+        '<div class="input-group-append">\n' +
+        '<button class="btn btn-danger modal-btn-replace-delete" type="button">\n' +
+        'Eliminar\n' +
+        '</button>\n' +
+        '</div>\n' +
+        '</div>\n' +
+        '</div>';
+}
+
+function btnEditFormAddModule(projectModuleId) {
+    var moduleObjects = getModuleObjects();
+    var moduleObj = getFormAddModuleObj();
+
+    for (var i = 0, len = moduleObjects.length; i < len; i++) {
+        if (moduleObjects[i]["projectModuleId"] == projectModuleId) {
+            moduleObjects[i] = moduleObj;
+            break;
+        }
+    }
+
+    updateProjectModules(moduleObjects);
+}
+
+function btnAddFormAddModule() {
+    var moduleObjects = getModuleObjects();
+    var moduleObj = getFormAddModuleObj();
+    var id = 1;
+
+    if (moduleObjects.length > 0) {
+        var lastModule = moduleObjects[moduleObjects.length - 1];
+        id = parseInt(lastModule["projectModuleId"]) + 1;
+    }
+
+    moduleObj["projectModuleId"] = id;
+
+    updateProjectModules(moduleObjects.concat(moduleObj));
+}
+
+function getModuleObjects() {
+    var modules = inputProjectModules.val();
+    var moduleObjects = [];
+
+    if (modules.length > 0) {
+        moduleObjects = JSON.parse(modules);
+    }
+
+    return moduleObjects;
+}
+
+function updateProjectModules(moduleObjects) {
+    inputProjectModules.val(JSON.stringify(moduleObjects));
+}
+
+function getFormAddModuleObj() {
+    updateTemplateReplace();
+
+    var moduleObj = {};
+    var modalFormSerialize = $(document).find('#modal-form-add-module').serializeArray();
+
+    for (var i = 0, len = modalFormSerialize.length; i < len; i++) {
+        var name = modalFormSerialize[i]["name"];
+        var value = modalFormSerialize[i]["value"];
+
+        moduleObj[name] = value;
+    }
+
+    return moduleObj;
+}
+
+function updateTemplateReplace() {
+    var modalFormAddModule = $(document).find('#modal-form-add-module');
+    var inputTemplateReplace = modalFormAddModule.find('#input-project-classes-template-replace');
+    var replaceObj = {};
+
+    modalFormAddModule.find('#nav-add-module-data > .form-group').each(function () {
+        var inputHidden = $(this).find('input[type=hidden]');
+        var inputText = $(this).find('input[type=text]');
+
+        if (inputHidden.length > 0 && inputText.length > 0) {
+            replaceObj[inputHidden.val()] = inputText.val()
+        }
+    });
+
+    inputTemplateReplace.val(JSON.stringify(replaceObj));
+}

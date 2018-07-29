114c7c6065b18dc9b2a5705be42008106fc20551
Nicolás Marulanda P <nicomaru@outlook.com>
2018-07-29 19:33:22 +0200

    Script common - Ahora es el que establece la versión del proyecto en la vista.

--- views/js/common.js
+++ views/js/common.js
@@ -1,6 +1,51 @@
 const {exec} = require('child_process');
 const fs = require('fs');
 
+let maxDirSearch;
+let elementProjectVersion;
+
+(function () {
+    maxDirSearch = 5;
+    elementProjectVersion = document.getElementById('project-version');
+    getAndSetVersion(__dirname);
+})();
+
+function getAndSetVersion(path) {
+    if (maxDirSearch === 0) {
+        return;
+    }
+
+    fs.readdir(path, (err, files) => {
+        --maxDirSearch;
+
+        if (err) {
+            return;
+        }
+
+        let fileJSONFilter = files.filter(value => value === 'package.json').toString();
+
+        if (fileJSONFilter.length === 0) {
+            getAndSetVersion(path + '/..');
+        } else {
+            fs.readFile(path + '/' + fileJSONFilter, 'utf8', (error, data) => {
+                if (error) {
+                    //TODO: como llamar a "module.exports.modalInformation"?
+                    $('#modal-information .modal-body .custom-pre').text('Error al obtener la versión de la app.\n' + error);
+                    $('#modal-information').modal('show');
+
+                    return;
+                }
+
+                elementProjectVersion.innerText = 'v' + JSON.parse(data).version;
+            });
+        }
+
+        if (maxDirSearch > 1) {
+            return;
+        }
+    });
+}
+
 module.exports = {
     resetElementForm: function (elementForm) {
         this.removeClassWasValidated(elementForm);
